d1ff79e72e336c80c95cd3bac812e05392fc9082
ANDRO <108537136+Andro2k@users.noreply.github.com>
2025-01-17 15:55:06 -0500

    nose que mas

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Selección de elementos
-const menuIcon = document.querySelector('#mobile-menu-icon');
-const mobileNavbar = document.querySelector('#mobile-navbar');
-const navLinks = document.querySelectorAll('.mobile-navbar li a');
+const menuIcon = document.querySelector("#mobile-menu-icon");
+const mobileNavbar = document.querySelector("#mobile-navbar");
+const navLinks = document.querySelectorAll(".mobile-navbar li a");
 
 // Alternar visibilidad del menú móvil
-menuIcon.addEventListener('click', () => {
-  mobileNavbar.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  mobileNavbar.classList.toggle("active");
 });
 
 // Cerrar el menú móvil al hacer clic en un enlace
 navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    mobileNavbar.classList.remove('active');
+  link.addEventListener("click", () => {
+    mobileNavbar.classList.remove("active");
   });
 });
